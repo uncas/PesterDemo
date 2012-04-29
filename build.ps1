3e6c8fbea442391b946fc26359217c5ccbf22f1e
Ole Lynge Soerensen <olelynge@gmail.com>
2012-04-29 15:20:11 +0200

    Add comments.

--- build.ps1
+++ build.ps1
@@ -1,9 +1,16 @@
+# Downloads Pester NuGet package to sub-folder packages:
 nuget install pester -o packages -s C:\NuGetPackages
+
+# Determines the path to the newest Pester package:
 $path = Resolve-Path .
 $pesterPackage = gci "$path\packages\Pester.*"
 if ($pesterPackage.count) {
   $pesterPackage = $pesterPackage[$pesterPackage.count-1]
 }
 $pesterPackagePath = $pesterPackage.FullName
+
+# Loads the module:
 Import-Module "$pesterPackagePath\tools\Pester.psm1"
+
+# Runs tests in sub-folder src:
 Invoke-Pester "$path\src"
